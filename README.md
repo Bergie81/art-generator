96d58dd2a9366c5ea8befa359b53be221721dea5
Thomas Bergmann <bergie81@gmail.com>
2021-09-07 22:18:38 +0200

    Add basic example

---
+++ README.md
@@ -0,0 +1,2 @@
+# Art Generator for NFTs
+_Node.js-based image generator that creates artworks based on provided layers._
--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const fs = require("fs");
 const { createCanvas, loadImage } = require("canvas");
+const { layers, width, height } = require("./input/config");
 
 const canvas = createCanvas(1000, 1000);
 const ctx = canvas.getContext("2d");
 
 const saveLayer = (_canvas) => {
   fs.writeFileSync("./newImage.png", _canvas.toBuffer("image/png"));
+  console.log("Image created");
 };
 
 const drawLayer = async () => {
   try {
     const image = await loadImage("./test-image.jpg");
-    ctx.drawImage(image, 0, 0, 1000, 1000); // image, x, y, width, height
+    ctx.drawImage(image, 0, 0, width, height); // draw image on the canvas: image, x, y, width, height
     console.log("ok");
+    saveLayer(canvas);
   } catch (error) {
     console.log("Error:", error);
   }
